811a580bdb19eacc235caf21ba3f4dda2b917324
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-05-07 17:49:22 +0530

    Create fire.js

---
+++ fire.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import fs from "fs/promises";
+import { MongoClient } from "mongodb";
+
+// MongoDB config
+const MONGO_URI = "mongodb+srv://mohanavamsi14:vamsi@cluster.74mis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+const DB_NAME = "stocksDB";
+const COLLECTION_NAME = "Web_links";
+
+async function connectToMongo() {
+  const client = new MongoClient(MONGO_URI);
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const collection = db.collection(COLLECTION_NAME);
+  return { client, collection };
+}
+
+const { collection } = await connectToMongo();
+
+let stocks = JSON.parse(await fs.readFile("./main_stocks.json", "utf-8"));
+
+let Symbols=await collection.find({}).toArray()
+Symbols=Symbols.map((i)=>{return i.symbol})
+for (let stock of stocks) {
+    if (Symbols.includes(stock.Symbol)) continue
+  if (!stock.Website) continue;
+
+  try {
+    const res = await axios.post(
+      "https://api.firecrawl.dev/v1/scrape",
+      { url: stock.Website, formats: ["links"] },
+      {
+        headers: {
+          Authorization: "Bearer fc-e2a73f0e071b48adadbcb6cf022f9626",
+        },
+      }
+    );
+
+    const links = res.data.data.links;
+    await collection.insertOne({
+      name: stock.Name,
+      symbol: stock.Symbol,
+      website: stock.Website,
+      links,
+    });
+
+    console.log(`Inserted data for ${stock.Name}`);
+  } catch (err) {
+    console.error(`Error processing ${stock.Name}:`, err.message);
+  }
+}
