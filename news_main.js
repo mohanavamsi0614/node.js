5c678ad64c7b37a1023e7cecf2a1b58b5b4f8598
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-05-22 16:07:21 +0530

    Create news_main.js

---
+++ news_main.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { MongoClient } from "mongodb";
+import fs from "fs/promises"
+import express from "express"
+
+const app=express()
+app.get("/",(req,res)=>{
+    res.send("working...")
+})
+app.listen(6000,()=>{
+    console.log("ewfj")
+})
+const MONGO_URI = "mongodb+srv://mohanavamsi14:vamsi@cluster0.3huumg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DB_NAME = "main_stock_list";
+const COLLECTION_NAME = "news";
+
+const client = MongoClient(MONGO_URI)
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const collection = db.collection(COLLECTION_NAME);
+
+
+let exist=await collection.find({}).toArray()
+exist=exist.map((i)=>i.symbol)
+const all_stocks=await fs.readFile("./main_stocks.json","utf-8")
+
+console.log("loaded")
+for (let stock of all_stocks){
+    if (exist.includes(stock.Symbol)) continue;
+    const {Industry,Name,Sector,Symbol}=stock
+    const api=`https://stockanalysis.com/fetch/infinitenews?type=s&symbol=${Symbol}`
+    let res=await axios.get(api)
+    res=res.data.data.map((i)=>{return {url:i.url,title:i.title}})
+    await collection.insertOne({
+        symbol:Symbol,
+        name:Name,
+        sector:Sector,
+        industry:Industry,
+        links:res
+    })
+    console.log("done for ",Name)
+}
+console.log("goood")
