afdafe38f887c9046ad01567e32ff28f197c50a7
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-05-23 16:01:08 +0530

    Update api.js

--- api.js
+++ api.js
@@ -4,14 +4,12 @@ import express from "express";
 const app=express()
 const client= new MongoClient("mongodb+srv://mohanavamsi14:vamsi@cluster0.3huumg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 await client.connect();
-const final=client["final_record"]
-const final_collection=final["main_stock_list"]
-
+const final=client.db("final_record").collection("main_stock_list")
 app.get("/",(req,res)=>{
     res.send("working...")
 })    
 app.get("/all",async(req,res)=>{
-    const all_stocks=await final_collection.find({}).toArray()
+    const all_stocks=await final.find({}).toArray()
     res.send(all_stocks)
 })
 app.listen(6000,()=>{
