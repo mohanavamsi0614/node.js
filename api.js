0129dc40da36a07578e55db2f8ef59544e4f6240
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-05-26 13:59:15 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,17 +1,26 @@
 import { MongoClient } from "mongodb";
 import express from "express";
 
-const app=express()
-const client= new MongoClient("mongodb+srv://mohanavamsi14:vamsi@cluster0.3huumg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-await client.connect();
-const final=client.db("final_record").collection("main_stock_list")
-app.get("/",(req,res)=>{
-    res.send("working...")
-})    
-app.get("/all",async(req,res)=>{
-    const all_stocks=await final.find({}).toArray()
-    res.send(all_stocks)
-})
-app.listen(6000,()=>{
-    console.log("ewfj")
-})    
+const app = express();
+const client = new MongoClient("mongodb+srv://mohanavamsi14:vamsi@cluster0.3huumg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+await client.connect(); // Top-level await only works with ES modules
+const final = client.db("final_record").collection("main_stock_list");
+
+app.get("/", (req, res) => {
+    res.send("working...");
+});
+
+app.get("/all", async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const skip = page * 100;
+    const all_stocks = await final.find({})
+                                   .skip(skip)
+                                   .limit(100)
+                                   .toArray();
+    res.send(all_stocks);
+});
+
+app.listen(6500, () => {
+    console.log("Server running on http://localhost:6500");
+});
