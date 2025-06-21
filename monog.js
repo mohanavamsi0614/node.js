b2f488b6fb426ec98ae93a2a424f1246f22e5047
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-06-21 15:47:54 +0530

    Create monog.js

---
+++ monog.js
@@ -0,0 +1,43 @@
+import { MongoClient } from 'mongodb';
+import express from "express"
+client=new MongoClient("mongodb+srv://mohanavamsi14:vamsi@cluster0.m8goeuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+await client.connect();
+const db=client.db("main_stock_list")
+const collection=db.collection("api_responses");
+
+const app = express();
+app.use(express.json());    
+app.get("/",(req,res)=>{
+    res.send("Welcome to the MongoDB API");
+})
+app.post("/get",async (req,res)=>{
+    const url=req.body.url;
+    const check=await collection.findOne({url: url
+    });
+    if(check){
+        res.json({
+            status: "success",
+        });
+    }        
+    res.json({
+        status: "not found",
+    });
+})
+app.post("/add",async (req,res)=>{
+    const {symbol,name,link,id}=req.body;
+    const check=await collection.insertOne({symbol, name, link, id});
+    if(check){
+        res.json({
+            status: "already exists",
+        });
+    } else {
+        await collection.insertOne({url: url});
+        res.json({
+            status: "success",
+        });
+    }
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
