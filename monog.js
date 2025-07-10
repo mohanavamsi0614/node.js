e2a7c5b0a9b3d21b5726a0db724cfd0cab6d0c44
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-07-10 21:48:46 +0530

    Update monog.js

--- monog.js
+++ monog.js
@@ -1,6 +1,10 @@
 import { MongoClient } from 'mongodb';
 import express from "express"
 const client=new MongoClient("mongodb+srv://mohanavamsi14:vamsi@cluster0.m8goeuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+const news_cllient=new MongoClient("mongodb+srv://mohanavamsi14:vamsi@cluster.74mis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster")
+ await news_client.connect(
+ const news_db=news_cllient.db("stocksDB")
+ const news_data=news_db.collection("Web_links_all_stock_list")
 await client.connect();
 const db=client.db("all_stock_list")
 const collection=db.collection("api_responses");
@@ -10,6 +14,11 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("Welcome to the MongoDB API");
 })
+app.post("/news",(req,res)=>{
+    const name=req.body.name;
+    const check = await  news_data.findOne({name:name})
+    res.json(check.links.slice(20))
+}
 app.post("/get",async (req,res)=>{
     const url=req.body.url;
     const check=await collection.findOne({url: url
