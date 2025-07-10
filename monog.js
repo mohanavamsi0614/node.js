a6286048741692875c8510b5fec88452d90e68f3
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-07-10 22:14:53 +0530

    Update monog.js

--- monog.js
+++ monog.js
@@ -2,9 +2,9 @@ import { MongoClient } from 'mongodb';
 import express from "express"
 const client=new MongoClient("mongodb+srv://mohanavamsi14:vamsi@cluster0.m8goeuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 const news_cllient=new MongoClient("mongodb+srv://mohanavamsi14:vamsi@cluster.74mis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster")
- await news_client.connect()
- const news_db=news_cllient.db("stocksDB")
- const news_data=news_db.collection("Web_links_all_stock_list")
+await news_client.connect()
+const news_db=news_cllient.db("stocksDB")
+const news_data=news_db.collection("Web_links_all_stock_list")
 await client.connect();
 const db=client.db("all_stock_list")
 const collection=db.collection("api_responses");
